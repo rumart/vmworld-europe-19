a4171b6eb6293b131e9dbe0158a63e598d9241f3
Rudi Martinsen <rudi.martinsen@gmail.com>
2019-11-04 16:14:18 +0100

    Adding script files

---
+++ appliance_info.ps1
@@ -0,0 +1,77 @@
+ <#
+    .SYNOPSIS
+        Script for outputting VCSA info to Influx
+    .DESCRIPTION
+        This script will fetch build, version and uptime from 
+        the VCSA REST API and post this data to a InfluxDB database
+
+        This script is tested against vCenter 6.7U1, please note that 
+        there might be differences in other version
+    .NOTES
+        Author: Rudi Martinsen
+        Created: 28/11-2018
+        Version: 1.0.0
+        Revised: 
+        Changelog:
+    .LINK
+        https://www.rudimartinsen.com/2018/12/03/vsphere-performance-vcenter-server-appliance-vcsa-monitoring/
+#>
+#Function for generating correct timestamp for influx input
+function Get-DBTimestamp($timestamp = (get-date)){
+    if($timestamp -is [system.string]){
+        $timestamp = [datetime]::ParseExact($timestamp,'dd.MM.yyyy HH:mm:ss',$null)
+    }
+    return $([long][double]::Parse((get-date $($timestamp).ToUniversalTime() -UFormat %s)) * 1000 * 1000 * 1000)
+}
+
+########################
+## Environment params ##
+########################
+$vcenter = "<your-vcenter-address>"
+$username = "<your-vc-user>"
+$pass = "<your-vc-pass"
+
+$database = "vcsa_demo"
+$influxServer = "<influxserver>"
+$influxPort = 8086
+
+################
+## End params ##
+################
+
+$BaseUri = "https://$vcenter/rest/"
+
+#Authenticate to vCenter
+$SessionUri = $BaseUri + "com/vmware/cis/session"
+$auth = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($UserName+':'+$Pass))
+$header = @{
+  'Authorization' = "Basic $auth"
+}
+$token = (Invoke-RestMethod -Method Post -Headers $header -Uri $SessionUri -SkipCertificateCheck).Value
+$sessionheader = @{'vmware-api-session-id' = $token}
+
+#Create array for storing data
+$tbl = @()
+
+#Fetch timestamp
+$timestamp = Get-DBTimestamp
+
+#API Endpoints
+$verUri = $BaseUri + "appliance/system/version"
+$upUri = $BaseUri + "appliance/system/uptime"
+
+#Fetch data
+$verResponse = Invoke-RestMethod -Method Get -Headers $sessionheader -Uri $verUri -SkipCertificateCheck
+$upResponse = Invoke-RestMethod -Method Get -Headers $sessionheader -Uri $upUri -SkipCertificateCheck
+
+#Build variables for Influx
+$version = $verResponse.value.version
+$build = $verResponse.value.build
+$uptime = $upResponse.value
+
+#Add to array
+$tbl += "appliance_info,server=$vcenter version=""$version"",build=$build,uptime=$uptime $timestamp"
+
+#Post data to Influx API
+$postUri = "http://$influxServer" + ":$influxPort/write?db=$database"
+Invoke-RestMethod -Method Post -Uri $postUri -Body ($tbl -join "`n") 
